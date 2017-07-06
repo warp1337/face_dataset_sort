a3bc827f6d3be91c25c52dd253a64dceb17459b9
fl <fl@techfak.uni-bielefeld.de>
2017-07-06 13:08:55 +0200

    Some convenience fixes

--- sort_data_set.py
+++ sort_data_set.py
@@ -18,6 +18,7 @@ f_cas = cv2.CascadeClassifier(c_path)
 c_pos = 0
 c_neg = 0
 c_all = 0
+c_skip = 0
 
 paths = [data_dir, pos_dir, neg_dir]
 
@@ -36,9 +37,14 @@ else:
 
 for im in glob.glob(data_dir + '/*.jpg'):
     c_all += 1
-    # print("Found: %s" % im)
+    print("Found: %s" % im)
     image = cv2.imread(im)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    try:
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    except Exception,e:
+        print "Skipping image %s" % im
+        c_skip += 1
+        continue
     # detect faces in the image
     faces = f_cas.detectMultiScale(
         gray,
@@ -47,18 +53,18 @@ for im in glob.glob(data_dir + '/*.jpg'):
         minSize=(30, 30)
     )
     if len(faces) > 0:
+        c_pos += 1
         print "Found {0} faces!".format(len(faces))
         for (x, y, w, h) in faces:
             cv2.rectangle(gray, (x, y), (x + w, y + h), (0, 255, 0), 2)
         cv2.imshow("Faces found", gray)
-        cv2.waitKey(0)
-        cv2.imwrite(pos_dir + "/" + im, gray)
-        c_pos += 1
+        cv2.waitKey(1)
+        cv2.imwrite(pos_dir + "/" + c_pos + ".jpg", image)
     else:
-        print "Writing negative example %s" % im
-        cv2.imwrite(neg_dir + "/" + im, gray)
         c_neg += 1
+        print "Writing negative example %s" % im
+        cv2.imwrite(neg_dir + "/" + c_neg + ".jpg", image)
     # save some space on disk
     os.remove(im)
 
-print "Processed %d images | positive %d | negative %d" % (c_all, c_pos, c_neg)
+print "Processed %d images | positive %d | negative %d | skipped %d" % (c_all, c_pos, c_neg, c_skip)
